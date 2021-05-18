f68131dbb3385ba15807653b15189b53b971f04c
EJ Fox <ejfox@ejfox.com>
2021-05-18 13:31:02 -0400

    feat: load csv and process to geojson featurecollection with turf

--- src/components/MapLayers.js
+++ src/components/MapLayers.js
@@ -1,10 +1,32 @@
 import React, { useContext } from "react";
 
 import GeoJsonLayer from "./GeoJsonLayer";
-import { Context } from "../store";
+import { Context, reducer } from "../store";
+import * as turf from "@turf/turf";
+import { csv } from "d3";
+const fetchData = () =>
+  csv("./data/facilities.csv", (d) => {
+    console.log(d);
+    return d;
+  });
+
+const facilitiesToGeoJSON = (facilities) => {
+  const facilityPoints = [];
+  facilities.map((f) => {
+    const point = turf.point([f.geo_latitude, f.geo_longitude], f);
+    facilityPoints.push(point);
+  });
+  return turf.featureCollection(facilityPoints);
+};
 
 const MapLayers = () => {
-  const { state } = useContext(Context);
+  const { state, dispatch } = useContext(Context);
+
+  // Load CSV, convert to GeoJSON, add to the layers in the store
+  fetchData().then((facilities) => {
+    const facilitiesGeoJSON = facilitiesToGeoJSON(facilities);
+    dispatch({ type: "add layer", payload: facilitiesGeoJSON });
+  });
   let enabledLayers = state.layers
     .filter(({ enabled }) => enabled)
     .sort((a, b) => {
@@ -22,7 +44,7 @@ const MapLayers = () => {
       before = enabledLayers[i - 1].id;
     }
 
-    if (layer.sourceType === 'geojson') {
+    if (layer.sourceType === "geojson") {
       return <GeoJsonLayer key={layer.id} layer={layer} before={before} />;
     }
     return null;
