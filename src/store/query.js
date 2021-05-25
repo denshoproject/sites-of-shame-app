109dac5a06e66e0baa234065c274aeec4fc33970
Eric Brelsford <ebrelsford@gmail.com>
2021-05-25 11:49:01 -0400

    Add enabled layers to URL

--- src/store/query.js
+++ src/store/query.js
@@ -1,8 +1,14 @@
 import queryString from "query-string";
 
 export const stateToQuery = (state) => {
+  const layers = state.layers
+    .filter((layer) => layer.enabled)
+    .map((layer) => layer.id)
+    .join("|");
+
   const queryState = {
     lat: state.mapState.center[1].toFixed(4),
+    layers,
     lng: state.mapState.center[0].toFixed(4),
     zoom: state.mapState.zoom[0],
   };
@@ -23,6 +29,14 @@ export const queryToState = (initialState) => {
     if (!stateUpdates.mapState)
       stateUpdates.mapState = { ...initialState.mapState };
     stateUpdates.mapState.center = [+queryState.lng, +queryState.lat];
+  }
+
+  if (queryState.layers) {
+    const enabledLayers = queryState.layers.split("|");
+    stateUpdates.layers = initialState.layers.map((layer) => ({
+      ...layer,
+      enabled: enabledLayers.indexOf(layer.id) >= 0,
+    }));
   }
 
   return {
