104edd08a0711d41aff40366b2f7078c7aeb5d02
Eric Brelsford <ebrelsford@gmail.com>
2021-06-07 15:53:56 -0400

    Re-orient layer picker to journeys and facilities

--- src/components/LayerPicker.js
+++ src/components/LayerPicker.js
@@ -8,39 +8,82 @@ import "./LayerPicker.scss";
 
 const LayerPicker = () => {
   const { state, dispatch } = useContext(Context);
-  // const layers = state.layers.filter(({ inPicker }) => inPicker);
   const layers = state.layers;
-  const handleClick = (layer) =>
-    dispatch({ type: "toggle layer", layerId: layer.id });
+
+  const enabledLayers = layers
+    .filter(({ enabled }) => enabled)
+    .map(({ id }) => id);
+
+  const toggleLayer = (id) => dispatch({ type: "toggle layer", layerId: id });
+  const isEnabled = (id) => enabledLayers.indexOf(id) >= 0;
+
+  const facilitiesLayer = layers.filter(({ id }) => id === "sos-facilities")[0];
 
   return (
     <div className="layer-picker">
-      <div className="layer-list">
-        {layers.map((layer) => (
-          <div
-            key={layer.id}
-            className={classNames({
-              layer: true,
-              enabled: layer.enabled,
-            })}
-          >
-            <div className="layer-name" onClick={() => handleClick(layer)}>
-              {layer.name}
-            </div>
-            {layer.id === "far" && layer.enabled ? <FARPicker /> : null}
-            {layer.layerLegend && layer.layerLegend.length ? (
-              <ol className="layer-legend-items">
-                {layer.layerLegend.map((legend) => (
-                  <li key={legend.color} className="layer-legend-item">
-                    <LegendCircle color={legend.color} />
-                    {legend.name}
-                  </li>
-                ))}
-              </ol>
-            ) : null}
+      <section className="layer-picker-section">
+        <h3>Journeys</h3>
+        <div className="layer-picker-section-body">
+          <div className="layer-picker-subsection">
+            <label>
+              <input
+                type="checkbox"
+                checked={isEnabled("sos-journeys")}
+                onChange={() => toggleLayer("sos-journeys")}
+              />
+              Family Journeys
+            </label>
           </div>
-        ))}
-      </div>
+          <div className="layer-picker-subsection">
+            <label>
+              <input
+                type="checkbox"
+                checked={isEnabled("far")}
+                onChange={() => toggleLayer("far")}
+              />
+              Final Accountability Records
+            </label>
+            <FARPicker />
+          </div>
+          <div className="layer-picker-subsection">
+            <label>
+              <input
+                type="checkbox"
+                checked={isEnabled("transfer orders")}
+                onChange={() => toggleLayer("transfer orders")}
+              />
+              Transfer orders
+            </label>
+          </div>
+        </div>
+      </section>
+      <section className="layer-picker-section">
+        <h3>Facilities</h3>
+        <div className="layer-picker-section-body">
+          <div className="layer-list">
+            {layers.map((layer) => (
+              <div
+                key={layer.id}
+                className={classNames({
+                  layer: true,
+                  enabled: layer.enabled,
+                })}
+              >
+                {layer.layerLegend && layer.layerLegend.length ? (
+                  <ol className="layer-legend-items">
+                    {layer.layerLegend.map((legend) => (
+                      <li key={legend.color} className="layer-legend-item">
+                        <LegendCircle color={legend.color} />
+                        {legend.name}
+                      </li>
+                    ))}
+                  </ol>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
