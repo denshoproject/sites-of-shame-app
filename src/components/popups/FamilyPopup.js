a022f2574683542202094b6a92852c63724f63dd
Heather Craig <hhcraig@gmail.com>
2021-06-15 09:02:42 -0400

    Update layout of family journey popups

--- src/components/popups/FamilyPopup.js
+++ src/components/popups/FamilyPopup.js
@@ -11,10 +11,25 @@ const FamilyPopup = ({ feature }) => {
         <span className="family-name">{properties.person_name}</span>
       </div>
       <div className="popup-row">
+        <span className="family-meta">
+          {/* <img alt="Image of Family Member" src={imagesrc} className="imgcrop" /> */}
+        </span>
+      </div>
+      <div className="popup-row">
         <span className="family-meta">{properties.family_name}</span>
       </div>
       <div className="popup-row">
         <span className="family-meta">{properties.individualdescription}</span>
+      </div>
+      <div className="popup-row">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="more-info"
+          //  href={}
+        >
+          More Info
+        </a>
       </div>
     </div>
   );
