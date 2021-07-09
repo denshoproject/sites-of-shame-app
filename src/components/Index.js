51b6e716d490333be222dda80d21600a9f3ce087
Heather Craig <hhcraig@gmail.com>
2021-07-09 10:20:11 -0400

    Add analytics

--- src/components/Index.js
+++ src/components/Index.js
@@ -5,14 +5,18 @@ import { Context } from "store";
 import InsetMap from "components/map/InsetMap";
 import MainMap from "components/map/MainMap";
 import "./Index.scss";
+import ReactGA from "react-ga";
 
 const Index = () => {
   const { dispatch } = useContext(Context);
+
+  ReactGA.initialize("UA-1762741-15");
 
   useEffect(() => {
     d3.csv("./infoboxes.csv").then((data) => {
       dispatch({ type: "set infobox content", content: data });
     });
+    ReactGA.pageview(window.location.pathname);
   }, [dispatch]);
 
   return (
